76c01694ec2508c92b4e09779eae1fd82e197d8f
Fernanda Ochoa <fernandaochoa@Fernandas-Mac.local>
2017-01-12 23:18:03 -0700

    add: If Statements

---
+++ Swift/If Statement.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//If Statements
+
+var userAge = 0
+
+userAge = 50
+
+if userAge < 30 {
+    print("you are young mister !")
+} else {
+    print("you are not young anymore ")
+}
+
+//Logical Statements
+
+// > mayor que
+// >= mayor o igual que
+// < menor que
+// <= menor o igual que
+// == exactamente igual
+// != diferente de
+// && and
+// || or
+
+var myName = "Fer"
+
+if myName == "Fer" {
+    print("tu nombre es  Paloma")
+} else if myName == "David" {
+    print ("tu nombre es Jose")
+} else if myName == "Desiree" {
+    print("tu nombre es dess")
+} else {
+    print("tu eres miembro de la familia ochoa")
+}
+
+var myNumber = 5
+
+if myNumber <= 6 {
+    print("my number is \(myNumber)")
+}
