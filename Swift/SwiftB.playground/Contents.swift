c625f99e5b0ad0f594b90d3a76f61f971632a093
Fernanda Ochoa <fernandaochoa@Fernandas-Mac.local>
2017-01-12 18:50:53 -0700

    add: Swift Basics

---
+++ Swift/SwiftB.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//String
+var name = "Ochoa"
+var surname = "Fernanda"
+
+var myString = "Apple"
+
+myString.append("s")
+
+myString.append(name)
+
+myString = "Pear"
+
+myString.lowercased()
+
+
+//Integer
+let firsNumber = 3.5
+
+//Double or Float
+let secondNumber = 3.5
+
+Double (firsNumber) + secondNumber
+
+//Boolean
+var bool = true
+bool = false
+
+
+//Kind of Variables
+
+//var yourString = "No" declarando un tipo
+var yourString : String
+
+//Ints
+let myInt : Int
+myInt = Int(10.45)
+
+//Double
+let myDouble : Double
+myDouble = 10.65
+
+
